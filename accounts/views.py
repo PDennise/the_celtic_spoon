0673ffd3d742c19862328375dadf6f56e9db75eb
Deniz Pandar <denizpandar@gmail.com>
2025-11-07 12:54:18 +0000

    Add auth views, URLs, and initial home view (pre-templates)

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm # Django built-in forms for user signup and login
 from django.contrib.auth import login, logout # Functions to log users in and out
+from django.contrib import messages         # Django's built-in messaging framework to show success or error messages to users
 
 # Create your views here.
 def register_view(request):
@@ -11,9 +12,9 @@ def register_view(request):
             user = form.save()                      # Save the new user to the database
             login(request, user)                    # Log the user in immediately
             messages.success(request, "Account created successfully!")
-            return redirect('home')                    # Redirect to the homepage after successful registration
+            return redirect('/')                    # Redirect to the homepage after successful registration
         else:
-            messages.error(request, "Please correcr the errors below.")  
+            messages.error(request, "Please correct the errors below.")  
     return render(request, 'accounts/register.html', {'form' : form}) # Render the registration template with the form (either empty or with errors)
 
 
@@ -24,7 +25,7 @@ def login_view(request):
         if form.is_valid():                         # Validate the form (check username and password)
             user = form.get_user()                  # Get the authenticated user object
             login(request, user)                    # Log the user in (start a session)
-            return redirect('home')                 # Redirect to the homepage after successful login
+            return redirect('/')                 # Redirect to the homepage after successful login
     return render(request, 'accounts/login.html', {'form': form}) #  Render the login template with the form (empty or with errors)
 
 
