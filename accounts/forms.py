b10aa58d60457e7afe496c0b0a7e1567b0578bb3
Deniz Pandar <denizpandar@gmail.com>
2025-11-10 16:30:17 +0000

    Create a registration form class

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,3 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+class ARegistrationForm(UserCreationForm):      # Inheriting from UserCreationForm
+    class Meta:                                 # Defines additional configurations for the form
+        model = User                            # Specify the model associated with this form
+        fields = ['username', 'email', 'password1', 'password2'] # Define the fields to include in the form
