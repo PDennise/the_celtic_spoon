e42e6374bc9721c9b30d378c4d22576944ac61e9
Deniz Pandar <denizpandar@gmail.com>
2025-11-10 17:17:14 +0000

    Fix register_view defination

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm # Django built-in forms for user signup and login
+from django.shortcuts import render, redirect
+from .forms import ARegistrationForm      # Import your custom user registration form
 from django.contrib.auth import login, logout # Functions to log users in and out
 from django.contrib import messages         # Django's built-in messaging framework to show success or error messages to users
 
 # Create your views here.
 def register_view(request):
-    form = UserCreationForm()                       # Create an empty user registration form
     if request.method == 'POST':                    # Check if the form was submitted
-        form = UserCreationForm(request.POST)       # Bind POST data to the form
+        form = ARegistrationForm(request.POST)      # Bind POST data to the form
         if form.is_valid():                         # Validate the form data
             user = form.save()                      # Save the new user to the database
             login(request, user)                    # Log the user in immediately
@@ -15,6 +14,8 @@ def register_view(request):
             return redirect('/')                    # Redirect to the homepage after successful registration
         else:
             messages.error(request, "Please correct the errors below.")  
+    else:
+        form = ARegistrationForm()                  # Create an empty user registration form
     return render(request, 'accounts/register.html', {'form' : form}) # Render the registration template with the form (either empty or with errors)
 
 
