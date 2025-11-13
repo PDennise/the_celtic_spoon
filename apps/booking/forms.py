c3720ad0e77931b2174c12a351175b7d65ffd8d8
Deniz Pandar <denizpandar@gmail.com>
2025-11-13 15:05:45 +0000

    Create booking forms.py

---
+++ apps/booking/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+form .models import Booking
+
+# Frontend form for users to make a booking
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ['customer', 'date', 'time', 'number_of_guests']  # Fields user can fill
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'}),   # HTML5 date picker
+            'time': forms.TimeInput(attrs={'type': 'time'}),   # HTML5 time picker
+        }
