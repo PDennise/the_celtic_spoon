a4ba425399c798ce1309155dd61d70b0c0d57174
Deniz Pandar <denizpandar@gmail.com>
2025-11-13 14:58:16 +0000

    Add models to admin.py

--- apps/booking/admin.py
+++ apps/booking/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from django import forms
+from .models import Booking
 # Register your models here.
+
+class BookingAdminForm(forms.ModelForm):            # Custom form for Booking to use HTML5 date and time pickers
+    class Meta:
+        model = Booking
+        fields = '__all__'
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'}), # HTML data picker
+            'time': forms.TimeInput(attrs={'type': 'date'}), # HTML time piker
+
+        }
+
+    
+    class BookingAdmin(admin.ModelAdmin):           # Admin configuration for Booking model
+        form = BookingAdminForm
+        list_display = ('customer', 'date', 'time', 'number_of_guests', 'created_at' )      # Columns to display in admin list view
+
+
+admin.site.register(Booking) # Register Booking model with the custom admin configuration
