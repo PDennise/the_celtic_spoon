afe93ba46e9061c198f88660b5c197ae14ab5176
Deniz Pandar <denizpandar@gmail.com>
2025-11-13 15:55:24 +0000

    edit urlpatterns

--- apps/booking/urls.py
+++ apps/booking/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns [
-    path('', views.create_booking, name= 'create_booking'),
+    path('create/', views.create_booking, name= 'create_booking'),  #  # URL for creating a booking
 ]
