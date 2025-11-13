212f7af144b32b1d6f7d680f67bbea6ee129b60c
Deniz Pandar <denizpandar@gmail.com>
2025-11-13 16:32:16 +0000

    Fix return at the bottom and @login_required at the top

--- apps/booking/views.py
+++ apps/booking/views.py
@@ -3,11 +3,11 @@ from .forms import BookingForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
-def create_booking(request):                      # @login_required  # User must be logged in
+@login_required  # User must be logged in
+def create_booking(request):                       
     if request.method == 'POST':
         form = BookingForm(request.POST)
-        if forms.is_valid():
+        if form.is_valid():
             booking = form.save(commit=False)
             booking.customer = request.user          # Automatically assign the logged-in user
             booking.save()
@@ -15,5 +15,5 @@ def create_booking(request):                      # @login_required  # User must
         
     else: 
         form = BookingForm()
-    return render(request, 'booking/create_booking.html', {form: 'form'})
+    return render(request, 'booking/create_booking.html', {'form' : form})
 
