6858ca8fde3b4a705a05219a9ac41418565db6e4
Deniz Pandar <denizpandar@gmail.com>
2025-12-02 22:58:33 +0000

    Update create_booking view to assign customer and redirect to my_bookings

--- apps/booking/views.py
+++ apps/booking/views.py
@@ -1,19 +1,22 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import BookingForm
 from django.contrib.auth.decorators import login_required
+from .models import Booking
 
-# Create your views here.
+# View to create a new booking
 @login_required  # User must be logged in
 def create_booking(request):                       
     if request.method == 'POST':
-        form = BookingForm(request.POST)
+        # Pass request.user to the form for validation
+        form = BookingForm(request.POST, user=request.user)
         if form.is_valid():
+            # Don't save to DB yet, we need to assign the customer
             booking = form.save(commit=False)
             booking.customer = request.user          # Automatically assign the logged-in user
-            booking.save()
-            return redirect('booking_success')       # Redirect after successful booking
+            booking.save()                           # Now save to DB
+            return redirect('my_bookings')       # Redirect to user's bookings page
         
     else: 
-        form = BookingForm()
+        form = BookingForm(user=request.user)
     return render(request, 'booking/create_booking.html', {'form' : form})
 
