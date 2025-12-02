00875f398570f44580ea139957801d151befaf1c
Deniz Pandar <denizpandar@gmail.com>
2025-12-02 23:13:26 +0000

    Add my bookings and cancel booking url paths

--- apps/booking/urls.py
+++ apps/booking/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('create/', views.create_booking, name= 'create_booking'),  #  # URL for creating a booking
+    path('mine/', views.my_bookings, name='my_bookings'),          # View user's bookings
+    path('cancel/<int:booking_id>/', views.cancel_booking, name='cancel_booking'),  # Cancel a booking
 ]
