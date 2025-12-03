9e651021ed54c5f47fb570e3fa0b25dcb6d7bccc
Deniz Pandar <denizpandar@gmail.com>
2025-12-03 23:02:26 +0000

    Add from django.contrib import messages

--- apps/booking/views.py
+++ apps/booking/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import BookingForm
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Booking
 
