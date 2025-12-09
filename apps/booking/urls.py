9c5b4783301c1a0734eb77821bcd5462ada73dfb
Deniz Pandar <denizpandar@gmail.com>
2025-12-09 21:51:56 +0000

    Add booking detail url and edit <int> in cancel path

--- apps/booking/urls.py
+++ apps/booking/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('create/', views.create_booking, name= 'create_booking'),  #  # URL for creating a booking
     path('my-bookings/', views.my_bookings, name='my_bookings'),          # View user's bookings
-    path('cancel/<int:booking_id>/', views.cancel_booking, name='cancel_booking'),  # Cancel a booking
+    path('booking/<int:pk>', views.booking_detail, name='booking_detail'), # View user's booking details
+    path('cancel/<int:pk>/', views.cancel_booking, name='cancel_booking'),  # Cancel a booking
 ]
