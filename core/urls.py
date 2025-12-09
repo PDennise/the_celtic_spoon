cec48f1e00fce40892c19ccc19fc1c1031806563
Deniz Pandar <denizpandar@gmail.com>
2025-12-09 22:54:49 +0000

    Add dasboard url

--- core/urls.py
+++ core/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('accounts/', include('apps.accounts.urls')),
     path('', views.home_view, name='home'), # home page
     path('restaurant/', include('apps.restaurant.urls')),
-    path('booking/', include('apps.booking.urls'),)
-
+    path('booking/', include('apps.booking.urls'),),
+    path('staff/dashboard/', views.staff_dashboard, name='staff_dashboard'),
 ]
