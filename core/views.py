f159c251509eff0d816fbda9dfe4f99b5e57105e
Deniz Pandar <denizpandar@gmail.com>
2025-12-09 22:55:18 +0000

    Add view for staff dashboard

--- core/views.py
+++ core/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import user_passes_test
+from apps.booking.models import Booking
+
+# Only staff users can access
+@user_passes_test(lambda u: u.is_staff)
+def staff_dashboard(request):
+    bookings = Booking.objects.all().order_by('-date') # get all bookings
+    context = {
+        'bookings': bookings
+    }
+
+    return render(request, 'staff_dashboard.html', context)
 
 def home_view(request):
     return render(request, 'home.html')
