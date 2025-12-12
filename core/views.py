352bc19805283b021f2e1bf1dfa47998fd63530a
Deniz Pandar <denizpandar@gmail.com>
2025-12-12 13:18:46 +0000

    Add approved booking view

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import user_passes_test
 from apps.booking.models import Booking
+from django.contrib import messages
 
 # Only staff users can access
 @user_passes_test(lambda u: u.is_staff)
@@ -11,6 +12,14 @@ def staff_dashboard(request):
     }
 
     return render(request, 'staff_dashboard.html', context)
+
+@user_passes_test(lambda u: u.is_staff)
+def approve_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    booking.status = 'approved'
+    booking.save()
+    messages.success(request, f"Booking #{booking.id} approved.")
+    return redirect('staff_dashboard')
 
 def home_view(request):
     return render(request, 'home.html')
